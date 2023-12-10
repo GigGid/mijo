2c6bd66101e3b38a632742ea51d73d39ac45f998
Gideon <rarasend100@gmail.com>
2023-12-10 19:59:36 +0000

    warnings fixed for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import AppRouter from "./lib/core/AppRouting/AppRouter";
 import "./custom.scss";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 
 function App() {
   return (
--- src/lib/presentation/pages/mainPages/home/homeForDesktops.tsx
+++ src/lib/presentation/pages/mainPages/home/homeForDesktops.tsx
@@ -2,8 +2,8 @@
 import './home.scss';
 import VideoGridSession from "./video-grid-session";
 import { OpportunitiesCardSession } from "./opportunities-card-session";
-import { Banner } from "./banner";
-import { Brands } from "./brands";
+// import { Banner } from "./banner";
+// import { Brands } from "./brands";
 import { VideoPlayerSession } from "./videoPlayerSession";
 
 function HomeForDesktops() {
